d623ed9e7033489fd2cb2a02ee7b2f5d4ef26cda
MrJWH <jh@wilner.dk>
2025-08-28 12:17:10 +0200

    Added DeviceRenamer files

---
+++ Application deployments/DeviceRenamer/DeviceRenamer.ps1
@@ -0,0 +1,133 @@
+﻿#region ---------------------------------------------------[Script parameters]-----------------------------------------------------
+param(
+    [Parameter(Mandatory=$true)]
+    [string]$CustomerPrefix
+)
+#endregion
+
+#region ---------------------------------------------------[Modifiable Parameters and defaults]------------------------------------
+[string]$CorpDataPath           = "C:\ProgramData\Microsoft\IntuneManagementExtension\Logs"
+[string]$ApplicationLogName = "#${CustomerPrefix}_DeviceRenamer"
+
+#endregion
+
+#region ---------------------------------------------------[Static Variables]------------------------------------------------------
+[string]$logpath = "$($CorpDataPath)"
+if (!(Test-Path -Path $logpath)) {
+    New-Item -Path $logpath -ItemType Directory -Force | Out-Null
+}
+[string]$Script:LogFile = "$($logpath)\$($ApplicationLogName).log"
+#endregion
+
+#region ---------------------------------------------------[Functions]-------------------------------------------------------------
+function Write-ToLog {
+
+    [CmdletBinding()]
+    param(
+        [Parameter()] [String] $LogMsg,
+        [Parameter()] [String] $LogColor = "White",
+        [Parameter()] [Switch] $IsHeader = $false
+    )
+
+    #Create file if doesn't exist
+    if (!(Test-Path $LogFile)) {
+        New-Item -ItemType File -Path $LogFile -Force | Out-Null
+
+        <#
+        #Set ACL for users on logfile
+        $NewAcl = Get-Acl -Path $LogFile
+        $identity = New-Object System.Security.Principal.SecurityIdentifier S-1-5-11
+        $fileSystemRights = "Modify"
+        $type = "Allow"
+        $fileSystemAccessRuleArgumentList = $identity, $fileSystemRights, $type
+        $fileSystemAccessRule = New-Object -TypeName System.Security.AccessControl.FileSystemAccessRule -ArgumentList $fileSystemAccessRuleArgumentList
+        $NewAcl.SetAccessRule($fileSystemAccessRule)
+        Set-Acl -Path $LogFile -AclObject $NewAcl
+        #>
+    }
+
+    #If header requested
+    if ($IsHeader) {
+        $Log = "################################################################`n         $(Get-Date -Format (Get-culture).DateTimeFormat.ShortDatePattern) $(Get-Date -UFormat "%T") -> $LogMsg`n################################################################"
+    }
+    else {
+        $Log = "$(Get-Date -UFormat "%T") - $LogMsg"
+    }
+
+    #Echo log
+    $Log | Write-host -ForegroundColor $LogColor
+
+    #Write log to file
+    $Log | Out-File -FilePath $LogFile -Append
+
+}
+
+#region ---------------------------------------------------[Script Execution]------------------------------------------------------
+
+#Configure console output encoding
+$null = cmd /c '' #Tip for ISE
+[Console]::OutputEncoding = [System.Text.Encoding]::UTF8
+$Script:ProgressPreference = 'SilentlyContinue'
+
+# ReadMe file with disclaimer and instructions
+$ReadMeFile = "$LogPath\README.txt"
+$ReadMeContent = @"
+This folder is used to manage all app installations as well as ongoing updates and maintenance. 
+
+
+##############################################
+It must not be deleted under any circumstances.
+##############################################
+"@
+
+# Create ReadMe file if it doesn't exist
+if (!(Test-Path $ReadMeFile)) {
+    $ReadMeContent | Out-File -FilePath $ReadMeFile
+}
+
+Write-ToLog "Starting installation script" -IsHeader
+Write-ToLog "-> Running as: $env:UserName"
+
+# Check if customerprefix is empty
+if (-not $CustomerPrefix) {
+    Write-ToLog "-> No customer prefix has been specified as an argument. The script is terminating." "Red"
+    exit 1
+}
+else {
+    Write-ToLog "-> Customer prefix: $CustomerPrefix"
+
+    # Collect serial number
+    $Serial = (Get-WmiObject Win32_BIOS).SerialNumber
+
+    # Collect current device name
+    $CurrentDeviceName = $env:COMPUTERNAME
+    Write-ToLog "-> Existing device name: $CurrentDeviceName"
+
+    # Check if serial number is longer than 8 characters and shorten it if necessary
+    if ($Serial.Length -gt 8) {
+    $Serial = $Serial.Substring(0, 8)
+    Write-ToLog "-> Serial number is longer than 8 characters!!"
+    Write-ToLog "-> The serial number has been shortened to: $Serial"
+    }
+
+    # Create new device name based on customer prefix and serial number
+    $NewDeviceName = $CustomerPrefix + "-" + $Serial
+
+    try {
+        Write-ToLog "-> Renaming device: $NewDeviceName"
+        # Rename computer
+        Rename-Computer -NewName $NewDeviceName
+        Write-ToLog "-> DeviceRenamer successfull" "Green"
+    }
+    catch {
+        # Write error to log
+        write-toLog "-> An error occurred while trying to rename the computer." "Red"
+        write-toLog "-> Error message: $_" "Red"
+        Write-ToLog "Ending installation script" -IsHeader
+        exit 1
+    }
+
+    Write-ToLog "Ending installation script" -IsHeader
+    # Return code 3010 // Intune sees this return code as "Soft Reboot"
+    return 3010
+}
---
+++ Application deployments/DeviceRenamer/DeviceRenamer_Detection_Palme3.ps1
@@ -0,0 +1,117 @@
+﻿#region ---------------------------------------------------[Modifiable Parameters and defaults]------------------------------------
+$Prefix = "Palme3"
+#endregion
+
+#region ---------------------------------------------------[Functions]-------------------------------------------------------------
+
+#region ---------------------------------------------------[Modifiable Parameters and defaults]------------------------------------
+[string]$CorpDataPath           = "C:\ProgramData\Microsoft\IntuneManagementExtension\Logs"
+[string]$ApplicationLogName = "#${Prefix}_DeviceRenamer"
+#endregion
+
+#region ---------------------------------------------------[Static Variables]------------------------------------------------------
+[string]$logpath = "$($CorpDataPath)"
+if (!(Test-Path -Path $logpath)) {
+    New-Item -Path $logpath -ItemType Directory -Force | Out-Null
+}
+[string]$Script:LogFile = "$($logpath)\$($ApplicationLogName).log"
+#endregion
+
+function Write-ToLog {
+
+    [CmdletBinding()]
+    param(
+        [Parameter()] [String] $LogMsg,
+        [Parameter()] [String] $LogColor = "White",
+        [Parameter()] [Switch] $IsHeader = $false
+    )
+
+    #Create file if it doesn't exist
+    if (!(Test-Path $LogFile)) {
+        New-Item -ItemType File -Path $LogFile -Force | Out-Null
+    }
+
+    #If header requested
+    if ($IsHeader) {
+        $Log = "################################################################`n         $(Get-Date -Format (Get-culture).DateTimeFormat.ShortDatePattern) $(Get-Date -UFormat "%T") -> $LogMsg`n################################################################"
+    }
+    else {
+        $Log = "$(Get-Date -UFormat "%T") - $LogMsg"
+    }
+
+    #Echo log
+    $Log | Write-host -ForegroundColor $LogColor
+
+    #Write log to file
+    $Log | Out-File -FilePath $LogFile -Append
+
+}
+
+function ApplicationDetected {
+    # Set STDOUT to a value so it is NOT empty
+    Write-Host "Random output to mark as installed"
+    # Return 0 // Marked as Installed
+    exit 0
+}
+function ApplicationNotDetected {
+    # Return 1 (STDOUT empty) // Marked as Not Installed
+    exit 1
+}
+
+#region ---------------------------------------------------[Script Execution]------------------------------------------------------
+
+#Configure console output encoding
+$null = cmd /c '' #Tip for ISE
+[Console]::OutputEncoding = [System.Text.Encoding]::UTF8
+$Script:ProgressPreference = 'SilentlyContinue'
+
+# ReadMe file with disclaimer and instructions
+$ReadMeFile = "$logpath\README.txt"
+$ReadMeContent = @"
+This folder is used to manage all app installations as well as ongoing updates and maintenance. 
+
+##############################################
+It must not be deleted under any circumstances.
+##############################################
+"@
+
+# Create ReadMe file if it doesn't exist
+if (!(Test-Path $ReadMeFile)) {
+    $ReadMeContent | Out-File -FilePath $ReadMeFile
+}
+
+Write-ToLog "Starting detection script" -IsHeader
+Write-ToLog "-> Running as: $env:UserName"
+
+# Get the current computer name
+$CurrentDeviceName = $env:COMPUTERNAME
+Write-ToLog "-> Current name: $CurrentDeviceName"
+
+# Get the serial number from BIOS
+$Serial = (Get-WmiObject Win32_BIOS).SerialNumber
+
+# Check if the serial number is longer than 8 characters and shorten it if necessary
+if ($Serial.Length -gt 8) {
+    $Serial = $Serial.Substring(0, 8)
+    Write-ToLog "-> Serial number is longer than 8 characters!!"
+    Write-ToLog "-> The serial number has been shortened to: $Serial"
+}
+else {
+    Write-ToLog "-> Serial number: $Serial"
+}
+
+# Define the new name with a dash
+$FinalDeviceName = $Prefix + "-" + $Serial
+Write-ToLog "-> Expected name: $FinalDeviceName"
+
+# Check if the computer name matches the expected name
+if ($FinalDeviceName -match $CurrentDeviceName) {
+    Write-ToLog "-> Computer name matches the expected name." "Green"
+    Write-ToLog "Ending detection script" -IsHeader
+    ApplicationDetected
+}
+else {
+    Write-ToLog "-> Computer name does not match the expected name." "Red"
+    Write-ToLog "Ending detection script" -IsHeader
+    ApplicationNotDetected
+}
