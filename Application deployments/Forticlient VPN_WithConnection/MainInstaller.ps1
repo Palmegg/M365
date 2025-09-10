e6af3362bdb151cd61812771ef4b8f78dfcc6ba0
MrJWH <jh@wilner.dk>
2025-09-10 14:11:57 +0200

    Now correct

--- Application deployments/Forticlient VPN_WithConnection/MainInstaller.ps1
+++ Application deployments/Forticlient VPN_WithConnection/MainInstaller.ps1
@@ -1,13 +1,14 @@
 #region ---------------------------------------------------[Script parameters]-----------------------------------------------------
-# Parameters removed by generator (values moved to Modifiable Parameters region)
+param(
+[Parameter(Mandatory=$false)][string]$Prefix,
+[Parameter(Mandatory=$false)][string]$VpnConnectionDescription,
+[Parameter(Mandatory=$false)][string]$VpnConfFileName,
+[Parameter(Mandatory=$false)][string]$Endpoint
+)
+
 #endregion
 
 #region ---------------------------------------------------[Modifiable Parameters and defaults]------------------------------------
-# Injected values by generator (can be changed)
-[string]$Prefix                       = "Ronald VPN"
-[string]$VpnConnectionDescription     = "Description"
-[string]$VpnConfFileName              = "RonaldTunnel"
-[string]$Endpoint                     = "vpn.ronald.dk:443"
 [string]$CorpDataPath                 = "C:\ProgramData\Microsoft\IntuneManagementExtension\Logs"
 [string]$ApplicationLogName           = "#${Prefix}_ForticlientInstaller"
 [string]$VpnConfFileName              = "$VpnConfFileName.reg"
